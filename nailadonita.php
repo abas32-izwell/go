cb05978d9283672450a0e9389b6d3903f93a2825
m sochabas <65498666+abas32-izwell@users.noreply.github.com>
2024-07-28 21:19:46 +0700

    Add files via upload

---
+++ nailadonita.php
@@ -0,0 +1,25 @@
+<?php
+ 
+$links = file('nailadonita.txt', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+
+if ($links === false || empty($links)) {
+    die('Tidak ada link yang tersedia.');
+}
+
+ 
+$links = array_filter($links);
+
+if (empty($links)) {
+    die('invalid.');
+}
+
+// Memutar array link secara acak
+shuffle($links);
+
+
+$selectedLink = $links[0];
+
+
+header("Location: $selectedLink");
+exit();
+?>
